c3eb72e0e3a402996fb13541ba4b59282822f2b9
ryanweiler92 <ryanweiler92@gmail.com>
2021-11-24 13:41:05 -0500

    module 3.1.7

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,20 @@ var fight = function() {
     enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
   // Log a resulting message to the console so we know that it worked.
     console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 };
